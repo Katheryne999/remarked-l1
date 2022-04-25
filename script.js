bb2a41342de0b561f1da31c50b0542e7e7d68b9d
Katheryne999 <fenix.kate1985@gmail.com>
2022-04-25 18:17:17 +0300

    index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('Hello everyone!');
+let studentName = prompt('What is your name'  );
+let studentHello= 'nice to meet you' + studentName;
+alert(studentHello);
+let studentAge = prompt('age' );
+let studentInfo = ' name: ' + studentName + ' age: ' + studentAge;
+alert(studentInfo);
